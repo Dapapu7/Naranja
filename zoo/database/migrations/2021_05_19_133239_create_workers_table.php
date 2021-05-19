8db42d6c3f24d93c5f2f7d84f2b8d8c33a9d10ce
jaimitxu <jaimitxu01@gmail.com>
2021-05-19 18:06:59 +0200

    jaime

--- zoo/database/migrations/2021_05_19_133239_create_workers_table.php
+++ zoo/database/migrations/2021_05_19_133239_create_workers_table.php
@@ -14,7 +14,7 @@ class CreateWorkersTable extends Migration
     public function up()
     {
         Schema::create('workers', function (Blueprint $table) {
-            $table->id('id_worker');
+            $table->bigInteger('id_worker')->primary()->autoIncrement();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('phone_number');
